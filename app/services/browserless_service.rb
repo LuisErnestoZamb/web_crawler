ff867c324b9625be3c8a47ccbefa2d84c42d0821
Luis Zambrano <luisezamb@gmail.com>
2025-03-26 22:46:07 -0400

    fixed lint issue

--- app/services/browserless_service.rb
+++ app/services/browserless_service.rb
@@ -2,13 +2,13 @@ class BrowserlessService
   include HTTParty
   base_uri = URI.parse(ENV.fetch("BROWSERLESS_DOMAIN", "https://production-sfo.browserless.io"))
 
-  def self.take_screenshot(url, options = {fullPage: true, type: 'png'})
+  def self.take_screenshot(url, options = { fullPage: true, type: "png" })
     response = post(
-      '/screenshot',
-      query: { token: ENV['BROWSERLESS_TOKEN'] },
+      "/screenshot",
+      query: { token: ENV["BROWSERLESS_TOKEN"] },
       headers: {
-        'Cache-Control' => 'no-cache',
-        'Content-Type' => 'application/json'
+        "Cache-Control" => "no-cache",
+        "Content-Type" => "application/json"
       },
       body: {
         url: url,
